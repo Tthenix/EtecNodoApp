a2515f29061ca7080f5223497681945a70d9eda9
Vitale Santino <s.vitale@etec.local>
2023-04-11 09:32:15 -0300

    perf: added comment in .env

--- backend/src/database.js
+++ backend/src/database.js
@@ -5,6 +5,8 @@ const URI = process.env.MONGODB_URI
             ? process.env.MONGODB_URI   
             : "mongodb://127.0.0.1/NodoApp"
 
+console.log(URI);
+
 mongoose.connect(URI)
     .then(db => console.log("DB funcando papa"))
     .catch(err => console.error(err));
